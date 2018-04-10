5fb28aa153af60e4567bb03e66aa70b7495488f3
fedeloscaltro <federico.scaltriti@libero.it>
2018-04-10 23:23:52 +0200

    Consenso posizione ok

--- app/src/main/java/com/gibbo/salvatore/MapsFragment.java
+++ app/src/main/java/com/gibbo/salvatore/MapsFragment.java
@@ -1,22 +1,44 @@
 package com.gibbo.salvatore;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.location.Address;
 import android.location.Geocoder;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -26,13 +48,14 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class MapsFragment extends Fragment implements OnMapReadyCallback{
+public class MapsFragment extends Fragment implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
 
+    private static final String TAG = MapsFragment.class.getSimpleName();
+    public static int MY_REQUEST_ACCESS_FINE_LOCATION = 1;
     //private OnFragmentInteractionListener mListener;
     GoogleMap mGoogleMap;
     MapView mMapView;
     View mView;
-    int c = 0;
 
     public MapsFragment() {
         // Required empty public constructor
@@ -41,10 +64,15 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }*/
+        if (checkFinePermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_REQUEST_ACCESS_FINE_LOCATION);
+        }
+
+    }
+
+    private boolean checkFinePermission(Context context, String accessPermission) {
+        return ContextCompat.checkSelfPermission(context, accessPermission)
+                != PackageManager.PERMISSION_GRANTED;
     }
 
 
@@ -61,7 +89,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback{
         super.onViewCreated(view, savedInstanceState);
 
         mMapView = (MapView) mView.findViewById(R.id.map);
-        if(mMapView != null){
+        if (mMapView != null) {
             mMapView.onCreate(null);
             mMapView.onResume();
             mMapView.getMapAsync(this);
@@ -73,6 +101,9 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback{
         MapsInitializer.initialize(getContext());
         mGoogleMap = googleMap;
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            mGoogleMap.setMyLocationEnabled(true);
+
 
         //gestisco azioni quando si clicca sulla mappa
         googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
@@ -91,7 +122,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback{
                 //seleziono la località da far apparire sopra il marker
                 try {
                     addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
-                    if(addresses.size()>0) {
+                    if (addresses.size() > 0) {
                         address = addresses.get(0).getAddressLine(0);
                         city = addresses.get(0).getLocality();
 
@@ -111,7 +142,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback{
             }
         });
     }
-    //per l'animazione che porta alla posizione selezionata
+
     private void pointToPosition(LatLng position) {
         //Build camera position
         CameraPosition cameraPosition = new CameraPosition.Builder()
@@ -119,5 +150,40 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback{
                 .zoom(10).build();
         //Zoom in and animate the camera.
         mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    private void ciao(){
+
+    }
+    protected void createLocationRequest() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(getContext());
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(getActivity(), new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                Log.i(TAG, "le impostazioni solo abilitate correttamente");
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == MY_REQUEST_ACCESS_FINE_LOCATION) {
+            if (permissions.length == 1 &&
+                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            } else {
+                // Permission was denied. Display an error message.
+            }
+        }
     }
 }
